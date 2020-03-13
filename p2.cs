a344b6789c894204513fd9bb88f0aa9d1642261f
tyo2685 <deny.prasetyo@students.amikom.ac.id>
2020-03-13 20:30:03 +0700

    Menambah Fungsi Penambahan

--- p2.cs
+++ p2.cs
@@ -1,8 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calculator
+{
 class Calculator
 {
     static void Main(string[] args)
     {
    
     }
-   
+   static int penambahan(int a, int b)
+    {
+        return a+b;
+    }
+}
 }
